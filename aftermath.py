1503da74b422b4cbe3c51494fbbbdb6ce97ab00f
leon <87223285+junyongleeme@users.noreply.github.com>
2022-05-12 16:50:30 +0900

    Add files via upload

---
+++ aftermath.py
@@ -0,0 +1,14 @@
+import json
+import os
+import set
+
+# 여러개의 json파일 통합
+def combine():
+    pass
+
+# trans폴더에서 직접 상태가 안좋은 wav파일 골라낸 후 실행하는 함수
+# 실행하면 json파일에서 삭제한 파일에 해당하는 "wavfile" : text를 삭제시킴
+def delete():
+    pass
+
+def 
